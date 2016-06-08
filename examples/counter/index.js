33eae4b2df5cd0ae5cec81fa21e2c37803fa09d3
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-08 20:54:19 +0300

    Update example to include class name prop

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -3,18 +3,36 @@
 import {createElement, render} from './../../dist/index.js';;
 
 let counter = 0;
+let lastActionClass = "";
+let lastAction = "";
 
 const counterJSX = (counter) => (
     <div>
+        <h2>Counter</h2>
         The number is: <span>{counter}</span>
+        <div>
+            The last action was... <span className={lastActionClass}>{lastAction}</span>
+        </div>
     </div>
 );
 
 const $root = document.getElementById('root');
+
 const $increase = document.getElementById('increase');
+const $decrease = document.getElementById('decrease');
 
 render($root, counterJSX(counter));
+
 $increase.addEventListener('click', () => {
     counter++;
+    lastAction = "Increase";
+    lastActionClass = "action-increase";
+    render($root, counterJSX(counter));
+});
+
+$decrease.addEventListener('click', () => {
+    counter--;
+    lastAction = "Decrease";
+    lastActionClass = "action-decrease";
     render($root, counterJSX(counter));
 });
